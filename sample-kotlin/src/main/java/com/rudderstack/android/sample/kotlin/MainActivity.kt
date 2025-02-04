d3065daf646a32ddf7be3f698fb66baf507bfe1c
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2025-02-04 10:55:34 +0530

    chore(sample-app): add sample events

--- sample-kotlin/src/main/java/com/rudderstack/android/sample/kotlin/MainActivity.kt
+++ sample-kotlin/src/main/java/com/rudderstack/android/sample/kotlin/MainActivity.kt
@@ -2,51 +2,28 @@ package com.rudderstack.android.sample.kotlin
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.rudderstack.android.sdk.core.RudderMessageBuilder
-import com.rudderstack.android.sdk.core.TrackPropertyBuilder
+import com.rudderstack.android.sdk.core.RudderProperty
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        sendEvents()
+        sendEvents()
     }
 
     private fun sendEvents() {
-        MainApplication.rudderClient!!.track(
-            RudderMessageBuilder()
-                .setEventName("daily_rewards_claim")
-                .setProperty(
-                    TrackPropertyBuilder()
-                        .setCategory("test_category")
-                        .build()
-                )
-        )
+        MainApplication.rudderClient.identify("RudderStack Android user id")
 
-        MainApplication.rudderClient!!.identify("developer_user_id")
+        MainApplication.rudderClient.track(
+            "Track event 1",
+            RudderProperty()
+                .putValue("key1", "value1")
+                .putValue("key2", 123)
+                .putValue("key3", true)
+                .putValue("key4", 4.56)
 
-        MainApplication.rudderClient!!.track(
-            RudderMessageBuilder()
-                .setEventName("level_up")
-                .setProperty(
-                    TrackPropertyBuilder()
-                        .setCategory("test_category")
-                        .build()
-                )
-        )
-
-        MainApplication.rudderClient!!.reset()
-
-        val revenueProperty = TrackPropertyBuilder()
-            .setCategory("test_category")
-            .build()
-        revenueProperty.put("total", 4.99)
-        revenueProperty.put("currency", "USD")
-
-        MainApplication.rudderClient!!.track(
-            RudderMessageBuilder()
-                .setEventName("revenue")
-                .setProperty(revenueProperty)
+                .putValue("revenue", 4.99)
+                .putValue("currency", "USD")
         )
     }
 }
